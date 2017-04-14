dca8053032b1dec4190159e645b84e6612c5dff8
Marcin Gierdalski <mgierdal@gmail.com>
2017-04-14 09:29:04 -0400

    CLI capability added

--- bookmarks_xml.py
+++ bookmarks_xml.py
@@ -11,19 +11,22 @@ from collections import Counter
 import re
 from time import time
 import argparse
+import datetime
 #import xml.etree.ElementTree as ET
 
+__version__ = '0.9'
 
-CURR_BMARK_FILE = r'C:\Users\mgier\AppData\Local\Google\Chrome\User Data\Default\Bookmarks'
-#CURR_BMARK_FILE = r'C:\Users\mgier\Documents\code\chrome_bookmarks\Bookmarks'
-#CURR_BMARK_FILE = r'C:\Users\mgier\Documents\code\chrome_bookmarks\Bookmarks.bak'
-CURR_BMARK_FILE = r'C:\porta\PortableApps\GoogleChromePortable\Data\profile\Default\bookmarks_4_4_17.html'
-#CURR_BMARK_FILE = r'C:\Users\mgier\AppData\Local\Google\Chrome\User Data\Default\bookmarks_4_6_17.html'
-CURR_BMARK_FILE = r'bookmarks_2_10_17.html'
-#CURR_BMARK_FILE = r'C:\Users\mgier\Documents\code\chrome_bookmarks\bookmarks_12_27_16.html'
+parser = argparse.ArgumentParser(description='Deduplicate bookmark file')
+parser.add_argument("bmark_in", help="HTML file to be deduplicated",
+                    type=str)
+parser.add_argument("--ndel", help="delete first N duplicated URLs",
+                    type=int)
+parser.add_argument("--verify", help="verify that no URL was lost",
+                    action="store_true")
+args = parser.parse_args()
 
 print 'input file',
-bm = open(CURR_BMARK_FILE, 'r').readlines()
+bm = open(args.bmark_in, 'r').readlines()
 print ' ... ingested'
 print 'XML size: {} lines'.format(len(bm))
 time_start = time()
@@ -46,18 +49,24 @@ duped_urls = [k for k,v in duped_urls]
 print '{} duplicated URLs'.format(len(duped_urls))
 print '{} deletions required'.format(deletions)
 
-
 time_decompose = time()
 count = 0
-for el in duped_urls[:100]:
+
+if args.ndel:
+    N = args.ndel
+else:
+    N = len(duped_urls)
+print 'deduplicating {} URLs'.format(N)
+for el in duped_urls[:N]:
     to_delete = tree.find_all(href=el)[1:]
     #print len(to_delete), el
     for unwanted in to_delete:
         unwanted.decompose()
         count = count + 1
-print '{} deletions performed in {} sec'.format(count, time() - time_decompose)
-print '{} sec per deletion'.format((time() - time_decompose)/count)
-
+time_spent = time() - time_decompose
+print '{} deletions performed in {} sec'.format(count, time_spent)
+print '{} sec per deletion'.format(time_spent/count)
+print 'EPT of {} deletions: {} '.format(deletions, datetime.timedelta(seconds=(time_spent/count) * deletions))
 
 #extract all URL entries, i.e. leaves whose keypath ends with 'url'
 url_list =  [a.attrs['href'] for a in tree.find_all('a')[:]]
@@ -65,30 +74,31 @@ url_list =  [a.attrs['href'] for a in tree.find_all('a')[:]]
 # sublist duplicated URLs, discounting administrative entries, i.e. containing 'chrome://' pseudo-URL 
 url_counter = Counter(url_list)
 duped_urls = [k for k,v in url_counter.iteritems() if (v>1) & (k.split(':')[0] != 'chrome')]
-print len(duped_urls)
+print '{} duplicated URLs'.format(len(duped_urls))
 
-deduped_fname = CURR_BMARK_FILE + '.deduped.html'
-print deduped_fname
+bmark_out = args.bmark_in + '.deduped.html'
+print 'output: {}'.format(bmark_out)
 orig_rec_limit = sys.getrecursionlimit()
 new_rec_limit = orig_rec_limit * 100
-sys.setrecursionlimit(new_rec_limit)
 print 'recursion limit changed from {} to {}'.format(orig_rec_limit, new_rec_limit)
+sys.setrecursionlimit(new_rec_limit)
 html = tree.prettify("utf-8")
 sys.setrecursionlimit(orig_rec_limit)
-with open(deduped_fname, "wb") as file:
+print 'recursion limit changed from {} to {}'.format(new_rec_limit, orig_rec_limit)
+with open(bmark_out, "wb") as file:
     file.write(html)
+	print '{} SAVED'.format(os.path.basename(bmark_out))
 
 ####################
-#extract all URL entries, i.e. leaves whose keypath ends with 'url'
-url_list = [a.attrs['href']
-            for a in bs4.BeautifulSoup(
-                reduce(lambda x,y:x+y, open(CURR_BMARK_FILE, 'r').readlines()), "lxml").find_all('a')[:]]
-print '{} individual URLs'.format(len(url_list))
-
-url_list_new = [a.attrs['href']
-            for a in bs4.BeautifulSoup(
-                reduce(lambda x,y:x+y, open(deduped_fname, 'r').readlines()), "lxml").find_all('a')[:]]
-print '{} individual URLs'.format(len(url_list_new))
-
-print 'individual URLs are the same: {}'.format(set(url_list) == set(url_list_new))
+if args.verify:
+    #extract all URL entries, i.e. leaves whose keypath ends with 'url'
+    url_list = [a.attrs['href']
+                for a in bs4.BeautifulSoup(
+                    reduce(lambda x,y:x+y, open(args.bmark_in, 'r').readlines()), "lxml").find_all('a')[:]]
+    print '{} individual URLs'.format(len(url_list))
+    url_list_new = [a.attrs['href']
+                for a in bs4.BeautifulSoup(
+                    reduce(lambda x,y:x+y, open(bmark_out, 'r').readlines()), "lxml").find_all('a')[:]]
+    print '{} individual URLs'.format(len(url_list_new))
+    print 'individual URLs are the same: {}'.format(set(url_list) == set(url_list_new))
 
