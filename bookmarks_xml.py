12a45d362bf49666cfadd0cd368f75283f8acbdb
Marcin Gierdalski <mgierdal@gmail.com>
2017-07-24 13:32:47 -0400

    switched to much faster direct processing of strings

--- bookmarks_xml.py
+++ bookmarks_xml.py
@@ -1,120 +1,196 @@
+#!/usr/bin/env python
+
 # coding: utf-8
 
-import os
-import sys
-import bs4
-#import lxml
-import xml
-#import json
+import os, sys
 from pprint import pprint
-from collections import Counter
 import re
-from time import time
+from collections import Counter 
+from itertools import tee, izip
 import argparse
-import datetime
-#import xml.etree.ElementTree as ET
 
-__version__ = '0.9.1'
+__version__ = "1.9.0"
+
+PTRN = r'<DT><A HREF="(.+?://.+?)"'
+
+def get_url_counter(bookmark_file_list, duped=False):
+    '''duped - if True, only multiplicated URLs are counted (default=False)'''
+    urls = [x[0] for x in [re.findall(PTRN, x) for x in bookmark_file_list] if x != []]
+    url_counter = Counter(urls)
+    if duped:
+        return Counter({k:v for k,v in list(url_counter.iteritems()) if v > 1})
+    else:
+        return url_counter
+    
+def first_bookmark_filter(entry, url_counter, ptrn):
+    '''TODO'''
+    bookmark = re.search(ptrn, entry)
+    if bookmark:
+        url = bookmark.group(1)
+        if url_counter[url] > 0: 
+            del url_counter[url]
+            return True
+        else:
+            return False
+    else:
+        return True
+
+def pairwise(iterable):
+    '''s -> (s0,s1), (s1,s2), (s2, s3), ...'''
+    a, b = tee(iterable)
+    next(b, None)
+    return izip(a, b)
+
+def delete_empty_folders_chrome(bookmarks):
+    '''TODO'''
+    indices = [i for i,v in enumerate(pairwise([x.strip() for x in bookmarks
+                                                if x.strip() > ''])) 
+               if ''.join(v) == '<DL><p></DL><p>']
+
+    for ix in indices:
+        print 'CH at index {} - folder {}, empty body {} {} '\
+              .format(ix,
+                      bookmarks[ix-1].strip(),
+                      bookmarks[ix+0].strip(),
+                      bookmarks[ix+1].strip())
+        # remove header of empty folder <DT><H3 ADD_DATE="1500646543" LAST_MODIFIED="1500646582">test1</H3>
+        bookmarks[ix-1] = ''
+        # remove empty folder '<DL><p>','</DL><p>'
+        bookmarks[ix+0], bookmarks[ix+1] = '',''
+        
+    return [x for x in bookmarks if x != '']
+
+def delete_empty_folders_ff(bookmarks):
+    '''TODO'''
+    indices = [i for i,v in enumerate(pairwise([x.strip() for x in bookmarks
+                                                if x.strip() > ''])) 
+               if ''.join(v) == '<DL><p></DL><p>']
+
+    for ix in indices:
+        print 'FF at index {} - folder {}, empty body {} {} '\
+              .format(ix,
+                      bookmarks[ix+0].strip(),
+                      bookmarks[ix+1].strip(),
+                      bookmarks[ix+2].strip())
+        # remove header of empty folder <DT><H3 ADD_DATE="1500646543" LAST_MODIFIED="1500646582">test1</H3>
+        bookmarks[ix+0] = ''
+        # remove empty folder '<DL><p>','</DL><p>'
+        bookmarks[ix+1], bookmarks[ix+2] = '',''
+        
+    return [x for x in bookmarks if x != '']
+
+def blank_empty_folder(bookmarks, indices):
+    '''TODO'''
+    # remove header of empty folder <DT><H3 ADD_DATE="1500646543" LAST_MODIFIED="1500646582">test1</H3>
+    bookmarks[indices[0]] = ''
+    # remove empty folder '<DL><p>','</DL><p>'
+    bookmarks[indices[1]], bookmarks[indices[2]] = '',''
+    
+def delete_empty_folders_gen(bookmarks):
+    '''TODO'''
+    START_TAG, END_TAG = '<DL><p>','</DL><p>'
+    indices = [i for i,v in enumerate(pairwise([x.strip() for x in bookmarks
+                                                if x.strip() > ''])) 
+               if ''.join(v) == ''.join([START_TAG, END_TAG])]
+    for ix in indices:
+        indices_tpl = (0,1,2)
+        if ((bookmarks[ix+1].strip() == START_TAG)
+            & (bookmarks[ix+2].strip() == END_TAG)):
+            print 'FF-like at index {} - folder {}, empty body {} {} '\
+                  .format(ix,
+                          bookmarks[ix+0].strip(),
+                          bookmarks[ix+1].strip(),
+                          bookmarks[ix+2].strip())
+            blank_empty_folder(bookmarks, indices_tpl)
+        elif ((bookmarks[ix+0].strip() == START_TAG)
+            & (bookmarks[ix+1].strip() == END_TAG)):
+            print 'CH-like at index {} - folder {}, empty body {} {} '\
+                  .format(ix,
+                          bookmarks[ix-1].strip(),
+                          bookmarks[ix+0].strip(),
+                          bookmarks[ix+1].strip())
+            # remove header of empty folder <DT><H3 ADD_DATE="1500646543" LAST_MODIFIED="1500646582">test1</H3>
+            bookmarks[ix-1] = ''
+            # remove empty folder '<DL><p>','</DL><p>'
+            bookmarks[ix+0], bookmarks[ix+1] = '',''
+        else:
+            msg = 'Cannot re-bind to empty folder at index {}'.format(ix)
+            raise ValueError(msg)
+    return [x for x in bookmarks if x != '']
+
+def main(argv):
+    ''''''
+    # TODO - ask around whether argument parsing 
+    # should be inside or outside the main() function
+    parser = argparse.ArgumentParser(
+        description='Deduplicate bookmark HTML file.')
+    parser.add_argument('infname',
+                        metavar='INPUT',
+                        type=str,
+                        help='input file name')
+    parser.add_argument('-b','--browser',
+                        metavar='BROWSER',
+                        choices=['ch', 'ff'],
+                        help='source of exported bookmarks (default: ???)')
+    parser.add_argument("-v", "--verbose",
+                        action='store_true',
+                        help="verbosity on")
+    args = parser.parse_args()
+    print 'Arguments:', args
+    INPUT = args.infname
+
+    if args.browser == 'ch':
+        global delete_empty_folders_chrome
+        delete_empty_folders = delete_empty_folders_chrome
+        print 'SRC BROWSER set to [{}]'.format(args.browser)
+    elif args.browser == 'ff':
+        global delete_empty_folders_ff
+        delete_empty_folders = delete_empty_folders_ff
+        print 'SRC BROWSER set to [{}]'.format(args.browser)
+    else:
+        delete_empty_folders = delete_empty_folders_gen
+        print 'SRC BROWSER {}'.format('autodetected')
+    #
+    # ingesting input HTML file
+    bm = open(INPUT, 'r').readlines()
+    # remove empty lines
+    print [x for x in bm if not x.strip(os.linesep).strip()]
+    bm = [x for x in bm if x.strip(os.linesep).strip()]
+    print '# of lines in input: {}'.format(len(bm))
+    # analysis of URLs
+    url_counter = get_url_counter(bm)
+    urls_list_input = list(url_counter)
+    print 'INPUT: {} unique URLs'.format(len(urls_list_input))
+##    # to count only multiplicated URLs
+##    print get_url_counter(bm, duped=True)
+    #
+    # letting pass only first instances of each URL
+    bm = [entry for entry in bm if first_bookmark_filter(entry, url_counter, PTRN)]
+    #
+    # diagnostic list of unique URLs
+    urls_list_deduped = list(get_url_counter(bm))
+    print 'DEDUPED: {} unique URLs'.format(len(urls_list_deduped))
+    #
+    # saving output with deduplicated URLs
+    with open(INPUT + '.deduped.html', 'w') as fout:
+        fout.writelines(bm)
+    #
+    # deleting empty bookmark folders
+    bm = delete_empty_folders_gen(bm)
+    #
+    # diagnostic list of unique URLs
+    urls_list_cleaned = list(get_url_counter(bm))
+    print 'CLEANED: {} unique URLs'.format(len(urls_list_cleaned))
+    #
+    # saving completely cleaned output
+    with open(INPUT + '.deduped.html' + '.deduped.html', 'w') as fout:
+        fout.writelines(bm)
+    #
+    # final diagnostics
+    assert urls_list_input == urls_list_deduped, \
+           'unique URLs differ between INPUT and DEDUPED'
+    assert urls_list_deduped == urls_list_cleaned, \
+           'unique URLs differ between DEDUPED and CLEANED'
 
 if __name__ == "__main__":
-    # setting CLI parser and parsing arguments
-    parser = argparse.ArgumentParser(description='Deduplicate bookmark file')
-    parser.add_argument("bmark_in", help="HTML file to be deduplicated",
-                        type=str)
-    parser.add_argument("--ndel", help="delete first N duplicated URLs",
-                        type=int)
-    parser.add_argument("--verify", help="verify that no URL was lost",
-                        action="store_true")
-    args = parser.parse_args()
-    
-    # ingesting input data
-    print 'input file',
-    bm = open(args.bmark_in, 'r').readlines()
-    print ' ... ingested'
-    print 'XML size: {} lines'.format(len(bm))
-    
-    # convert XML to a soup, after joining text lines into 1 long string
-    # TODO compare time for joining by 'reduce()' with 'str.join()'
-    time_start = time()
-    print 'XML',
-    tree = bs4.BeautifulSoup(reduce(lambda x,y:x+y, bm), "lxml")
-    print ' ... parsed in {} sec'.format(time() - time_start)
-    print '{} sec per line'.format((time() - time_start)/len(bm))
-
-    #extract all URL entries, i.e. leaves whose keypath ends with 'url'
-    url_list =  [a.attrs['href'] for a in tree.find_all('a')[:]]
-    print '{} individual URLs'.format(len(url_list))
-
-    # sublist duplicated URLs, discounting administrative entries,
-    # i.e. containing 'chrome://' pseudo-URL 
-    url_counter = Counter(url_list)
-    duped_urls = [(k,v) for k,v in url_counter.iteritems()
-                  if (v>1) & (k.split(':')[0] != 'chrome')]
-    deletions = sum([v for k,v in duped_urls])
-    duped_urls = [k for k,v in duped_urls]
-    print '{} duplicated URLs'.format(len(duped_urls))
-    print '{} deletions required'.format(deletions)
-
-    time_decompose = time()
-    count = 0
-
-    # limit number of actual deletions to perform (e.g. for testing)
-    if args.ndel:
-        N = args.ndel
-    else:
-        N = len(duped_urls)
-    print 'deduplicating {} URLs'.format(N)
-    
-    # actual deletions
-    # TODO: very resource consuming, try deleting from original XML
-    for el in duped_urls[:N]:
-        to_delete = tree.find_all(href=el)[1:]
-        #print len(to_delete), el
-        for unwanted in to_delete:
-            unwanted.decompose()
-            count = count + 1
-    time_spent = time() - time_decompose
-    print '{} deletions performed in {} sec'.format(count, time_spent)
-    print '{} sec per deletion'.format(time_spent/count)
-    print 'EPT of {} deletions: {} '.format(deletions, datetime.timedelta(seconds=(time_spent/count) * deletions))
-
-    #extract all URL entries, i.e. leaves whose keypath ends with 'url'
-    url_list =  [a.attrs['href'] for a in tree.find_all('a')[:]]
-
-    ############
-    # TODO: delete empty folders
-    ############
-    
-    # rechecking that all duplications were deleted
-    # sublist duplicated URLs, discounting administrative entries, i.e. containing 'chrome://' pseudo-URL 
-    url_counter = Counter(url_list)
-    duped_urls = [k for k,v in url_counter.iteritems() if (v>1) & (k.split(':')[0] != 'chrome')]
-    print '{} duplicated URLs'.format(len(duped_urls))
-
-    # saving deduplicated output
-    bmark_out = args.bmark_in + '.deduped.html'
-    print 'output: {}'.format(bmark_out)
-    orig_rec_limit = sys.getrecursionlimit()
-    new_rec_limit = orig_rec_limit * 100
-    print 'recursion limit changed from {} to {}'.format(orig_rec_limit, new_rec_limit)
-    sys.setrecursionlimit(new_rec_limit)
-    html = tree.prettify("utf-8")
-    sys.setrecursionlimit(orig_rec_limit)
-    print 'recursion limit changed from {} to {}'.format(new_rec_limit, orig_rec_limit)
-    with open(bmark_out, "wb") as file:
-        file.write(html)
-        print '{} SAVED'.format(os.path.basename(bmark_out))
-
-    # verify that no URL was lost
-    if args.verify:
-        #extract all URL entries, i.e. leaves whose keypath ends with 'url'
-        url_list = [a.attrs['href']
-                    for a in bs4.BeautifulSoup(
-                        reduce(lambda x,y:x+y, open(args.bmark_in, 'r').readlines()), "lxml").find_all('a')[:]]
-        print '{} individual URLs'.format(len(url_list))
-        url_list_new = [a.attrs['href']
-                    for a in bs4.BeautifulSoup(
-                        reduce(lambda x,y:x+y, open(bmark_out, 'r').readlines()), "lxml").find_all('a')[:]]
-        print '{} individual URLs'.format(len(url_list_new))
-        print 'individual URLs are the same: {}'.format(set(url_list) == set(url_list_new))
-
+    main(sys.argv)
